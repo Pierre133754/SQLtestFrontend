fee1234d26663ece3b12d9f823a96e01a2c25a22
Big Man <froxyopro@yahoo.fr>
2024-09-12 20:22:35 +0200

    first commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+
+export default function Test() {
+    const [comments, setComments] = useState()
+    
+    useEffect(() => {
+        fetch(import.meta.env.VITE_API+'/comments').then(res => res.json()).then(data => setComments(data))
+    },[])
+
+    return (
+        <>
+            <div className="input">
+                <form className="inputForm" onSubmit={async (e) => {
+                    e.preventDefault()
+                    const user = document.getElementById('user').value
+                    const comment = document.getElementById('comment').value
+                    await fetch(import.meta.env.VITE_API+'/comments', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            user: user,
+                            comment: comment
+                        })
+                    })
+                    fetch(import.meta.env.VITE_API+'/comments').then(res => res.json()).then(data => setComments(data))
+                }}>
+                    <input type="text" id="user" name="user" placeholder="Username"></input>
+                    <textarea id="comment" name="comment" placeholder="Message" rows={5} cols={50}></textarea>
+                    <input type="submit"></input>
+                </form>
+            </div>
+            <div className="comments">
+                {
+                    comments?.map((comment) => {
+                        return (
+                            <div className="commentsWhole" key={comment.id}>
+                                <p className="commentsWholeUser">{comment.user}</p>
+                                <p className="commentsWholeComment">{comment.comment}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
